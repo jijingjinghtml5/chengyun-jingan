331d46649bbe135632c6d4a0dc632aecacceabe0
shengjun <shengjun@zamplus.com>
2020-11-23 16:27:06 +0800

    update

--- src/modules/TownSummary/appraise.vue
+++ src/modules/TownSummary/appraise.vue
@@ -1,3 +1,76 @@
 <template>
-  <div>appraise</div>
+  <div>
+    <chart-panel txt="街镇得分" :chartData="townData" class="panel"></chart-panel>
+    <chart-panel txt="部门得分" :chartData="departmentData" class="panel"></chart-panel>
+    <chart-panel txt="居民区得分" :chartData="townData" class="panel"></chart-panel>
+    <wrap-title txt="人员得分" :level="2" icon="icon-biaoti" class="table-container">
+      <m-list style="height: 100%;"
+        :headers="headers"
+        :dataset="tableData"
+      ></m-list>
+    </wrap-title>
+  </div>
 </template>
+<script>
+import WrapTitle from "@/components/MTitle/WrapTitle";
+import MList from "@/components/MList/index";
+import ChartPanel from "./chart";
+export default {
+  name: "Appraise",
+  components: { ChartPanel, WrapTitle, MList },
+  data() {
+    return {
+      townData: [
+        ["时间", "南西", "静安寺", "石门二", "芷江西", "大宁路", "共和新", "彭浦镇", "临汾", "江宁路", "宝山路", "彭浦新村", "天目西"],
+        ["2020.06", Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
+        ["2020.07", Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
+        ["2020.08", Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
+        ["2020.09", Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
+        ["2020.10", Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)]
+      ],
+      departmentData: [
+        ["时间", "自治办", "管理办", "服务办", "房管办", "平安办", "市容所", "市场监督", "城管中队", "发展办", "物业"],
+        ["2020.06", Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
+        ["2020.07", Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
+        ["2020.08", Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
+        ["2020.09", Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
+        ["2020.10", Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)]
+      ],
+      headers: [
+        {
+          label: "姓名",
+          prop: "name"
+        },
+        {
+          label: "所属街镇",
+          prop: "town"
+        },
+        {
+          label: "所属部门",
+          prop: "department"
+        },
+        {
+          label: "得分",
+          prop: "score"
+        }
+      ],
+      tableData: [
+        { id: 1, name: "张建", town: "南京西路街道", department: "市容所", score: Math.floor(Math.random() * 100) },
+        { id: 2, name: "张建", town: "南京西路街道", department: "市容所", score: Math.floor(Math.random() * 100) },
+        { id: 3, name: "张建", town: "南京西路街道", department: "市容所", score: Math.floor(Math.random() * 100) },
+        { id: 4, name: "张建", town: "南京西路街道", department: "市容所", score: Math.floor(Math.random() * 100) },
+        { id: 5, name: "张建", town: "南京西路街道", department: "市容所", score: Math.floor(Math.random() * 100) },
+        { id: 6, name: "张建", town: "南京西路街道", department: "市容所", score: Math.floor(Math.random() * 100) }
+      ]
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+.panel{
+  margin-bottom:20px;
+}
+.table-container{
+  height: 490px;
+}
+</style>
---
+++ src/modules/TownSummary/chart.vue
@@ -0,0 +1,58 @@
+<template>
+<wrap-title :txt="txt" :level="2" icon="icon-biaoti" class="chart-container">
+  <chart-line :chartData="chartData"  :colors="colors" :showYLabel="true" :isArea="false" :isSmooth="false" :showLegend="true"></chart-line>
+</wrap-title>
+</template>
+<script>
+import WrapTitle from "@/components/MTitle/WrapTitle";
+import ChartLine from "../../components/Charts/Line/ChartLine.vue";
+
+export default {
+  components: { ChartLine, WrapTitle },
+  name: "ChartPanel",
+  props: {
+    txt: {
+      type: String,
+      default: ""
+    },
+    chartData: {
+      type: Array,
+      default: () => {
+        return [];
+      }
+    },
+    colors: {
+      type: Array,
+      default: () => {
+        return [
+          "#30BC9B",
+          "#4FCFD5",
+          "#F96F4F",
+          "#FCBF51",
+          "#E64C3B",
+          "#F23470",
+          "#BE6CCC",
+          "#989898",
+          "#4E78A4",
+          "#154FA3",
+          "#1D7774",
+          "#FB9EA9",
+          "#AD7A9F",
+          "#A57179",
+          "#5458A9",
+          "#F18F3A",
+          "#E0595C",
+          "#76B7B0",
+          "#59A055",
+          "#B9B1AC"
+        ];
+      }
+    }
+  }
+};
+</script>
+<style lang="postcss" scoped>
+.chart-container{
+  height: 424px;
+}
+</style>
