e3034a0f57c0ecf277b89506870babf86cd1a7aa
liupei <liupei@chinadep.com>
2022-12-13 13:40:26 +0800

    feat：页面修改

--- src/components/popups/page1/PubliceTransportPopup.vue
+++ src/components/popups/page1/PubliceTransportPopup.vue
@@ -10,10 +10,10 @@
   </div>
 </template>
 <script>
-import MList from "@/components/MList/index.vue";
-import { getPublicLine } from "../api.js";
+import MList from '@/components/MList/index.vue'
+import { getPublicLine } from '../api.js'
 export default {
-  name: "PubliceTransportPopup",
+  name: 'PubliceTransportPopup',
   components: {
     MList
   },
@@ -21,88 +21,88 @@ export default {
     info: {
       type: Object,
       default: () => {
-        return {};
+        return {}
       }
     }
   },
-  data() {
+  data () {
     return {
       headers: [
         {
-          label: "线路名称",
-          prop: "name",
-          align: "center"
+          label: '线路名称',
+          prop: 'name',
+          align: 'center'
         }
       ],
       tableData: [],
       publicLineLayer: null
-    };
+    }
   },
-  created() {
-    this.registerLayer();
+  created () {
+    this.registerLayer()
     getPublicLine().then(res => {
       this.tableData = (res.data.messages || []).map((item, index) => {
         return {
-          name: item.args.attributes.NAME,
+          name: `${item.args.attributes.NAME}( ${item.args.attributes.DIRECTION} )`,
           ...item,
           id: index
-        };
-      });
-    });
+        }
+      })
+    })
   },
   methods: {
-    registerLayer() {
-      this.publicLineLayer = this.$_mapProxy.registerLayer("publicLineLayer", "公交线路", this.$_mapProxy.MANUAL_GROUP, {
-        "name": "publicLineLayer",
-        "type": "polyline",
-        "data": {
-          "data": [],
-          "parsegeometry": "function(item){return {paths:item.data.path.points}}"
+    registerLayer () {
+      this.publicLineLayer = this.$_mapProxy.registerLayer('publicLineLayer', '公交线路', this.$_mapProxy.MANUAL_GROUP, {
+        'name': 'publicLineLayer',
+        'type': 'polyline',
+        'data': {
+          'data': [],
+          'parsegeometry': 'function(item){return {paths:item.data.path.points}}'
         },
-        "legendVisible": true,
-        "popupEnabled": true,
-        "renderer": {
-          "type": "simple",
-          "label": "公交线路",
-          "visualVariables": [],
-          "symbol": {
-            "type": "line-3d",
-            "symbolLayers": [
+        'legendVisible': true,
+        'popupEnabled': true,
+        'renderer': {
+          'type': 'simple',
+          'label': '公交线路',
+          'visualVariables': [],
+          'symbol': {
+            'type': 'line-3d',
+            'symbolLayers': [
               {
-                "type": "line",
-                "size": 3,
-                "material": {
-                  "color": "red"
+                'type': 'line',
+                'size': 3,
+                'material': {
+                  'color': 'red'
                 },
-                "cap": "round",
-                "join": "round"
+                'cap': 'round',
+                'join': 'round'
               }
             ]
           }
         }
-      });
+      })
     },
-    handleRowClick(row) {
+    handleRowClick (row) {
       if (row) {
         this.publicLineLayer.setParameters({
-          "data": {
-            "content": [row],
-            "parsegeometry": "function(item){return {paths:item.data.path[0].points}}"
+          'data': {
+            'content': [row],
+            'parsegeometry': 'function(item){return {paths:item.data.path[0].points}}'
           }
-        }).open();
+        }).open()
       } else {
-        this.publicLineLayer.close();
+        this.publicLineLayer.close()
       }
     }
   },
-  beforeDestroy() {
-    this.publicLineLayer.close();
+  beforeDestroy () {
+    this.publicLineLayer.close()
   }
-};
+}
 </script>
 <style lang="scss" scoped>
 .panel{
-  width: 6rem;
+  width: 10rem;
   height: 15.6rem;
   padding: 0.4rem;
   border: 1px solid #4E78A4;
